ea94e1de49b5a9ab280f7b78fa4b078ae0a5de36
delerion <nelexeon@gmail.com>
2022-09-12 17:45:28 +0300

    build

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import "./index.css";
 import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter >
     <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
